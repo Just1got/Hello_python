b6b04a9a7185b29aeeb5e318d1b4b4fef7715dd2
Just1got <goda1udyt@gmail.com>
2023-01-26 00:46:52 +0300

    mistakes
    Rework mistakes

--- HM3/main.py
+++ HM3/main.py
@@ -5,15 +5,15 @@
 
 #1) Из строки «Python is the best programming language in the world» получить подстроку начиная с 6 символа с начала
 # исходной строки и до 7 символа с конца исходной строки (нумерация символов начинается с нуля).
-s = "Python is the best programming language in the world"
-print(s[6:-7])
+s1 = "Python is the best programming language in the world"
+print(s1[5:-7])
 
 #2) Вывести каждый третий символа строки «Guido van Rossum is the benevolent dictator for life».
-print("Guido van Rossum is the benevolent dictator for life"[::3])
+print("Guido van Rossum is the benevolent dictator for life"[2::3])
 
 #4) Из строки «You have a problem with authority, Mr. Anderson.» получить словарь, где ключ -это символ, а значение -
 # это количество повторений символа в строке.
-s = "You have a problem with authority, Mr. Anderson."
-g = dict(zip(list(s), list(map(s.count,list(s)))))
+s2 = "You have a problem with authority, Mr. Anderson."
+g = dict(zip(list(s2), list(map(s2.count,list(s2)))))
 print(g)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
